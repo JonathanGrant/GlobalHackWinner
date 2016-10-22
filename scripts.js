8595af57bf50c8c7468e68d44650a1436b695596
JonathanGrant <jonathanallengrant@gmail.com>
2016-10-22 08:29:37 -0700

    Added basic grid

--- scripts.js
+++ scripts.js
@@ -8,7 +8,40 @@ var Camera = React.createClass({
     return (
         <a-camera position={this.state.pos}>
             <a-cursor color="#FF0000"></a-cursor>
+            <a-animation attribute="position" from="0 0 0" to="0 5 0" duration="1000"></a-animation>
         </a-camera>
+        )
+  }
+})
+
+var Cube = React.createClass({
+  render: function() {
+    return (
+        <a-box position={this.props.pos}>
+        </a-box>
+        )
+  }
+})
+
+var Sphere = React.createClass({
+  render: function() {
+    return (
+        <a-sphere position={this.props.pos} radius={this.props.radius} color={this.props.theColor}>
+        </a-sphere>
+        )
+  }
+})
+
+var Sky = React.createClass({
+  getInitialState: function() {
+    return {
+      pos: '1 1 -4'
+    }
+  },
+  render: function() {
+    return (
+        <a-sky src="./sky.jpg">
+        </a-sky>
         )
   }
 })
@@ -18,6 +51,16 @@ var AFrameScene = React.createClass({
     return (
       <a-scene>
         <Camera />
+        <Sky />
+        <Cube pos="1 5 -4" />
+        <Cube pos="1 6 -4" />
+        <Cube pos="1 4 -4" />
+        <Cube pos="0 6 -4" />
+        <Sphere pos="0 5 -4" radius="0.25" theColor="purple"/>
+        <Cube pos="0 4 -4" />
+        <Cube pos="-1 5 -4" />
+        <Cube pos="-1 6 -4" />
+        <Cube pos="-1 4 -4" />
       </a-scene>)
   }
 })
