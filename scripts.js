684075f41863df338defb1176226366e379924e4
JonathanGrant <jonathanallengrant@gmail.com>
2016-10-22 07:47:12 -0700

    Got the cursor to load

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var Camera = React.createClass({
+  getInitialState: function() {
+    return {
+      pos: '0 0 0'
+    }
+  },
+  render: function() {
+    return (
+        <a-camera position={this.state.pos}>
+            <a-cursor color="#FF0000"></a-cursor>
+        </a-camera>
+        )
+  }
+})
+
+var AFrameScene = React.createClass({
+  render: function() {
+    return (
+      <a-scene>
+        <Camera />
+      </a-scene>)
+  }
+})
+
+ReactDOM.render(<AFrameScene />, document.getElementById('container'))
